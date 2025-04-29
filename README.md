b1fdaddde50f5b9dc60ad9d639cedb76731878e4
nabeelmohammed1 <64881066+nabeelmohammed1@users.noreply.github.com>
2025-04-29 09:54:44 -0400

    hjhj

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdvancedProgramming
+
+Developed with Unreal Engine 5
---
+++ Source/AdvancedProgramming/AdvancedProgrammingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvancedProgrammingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAdvancedProgrammingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAdvancedProgrammingGameMode();
+};
+
+
+
